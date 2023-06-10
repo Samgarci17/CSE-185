2b4006b0522d28f3d2dc90e281901b4f4973c831
Samuel Garcia <sag086@ucsd.edu>
2023-06-09 20:01:43 -0700

    Updated File structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 curdir = os.path.abspath(os.path.dirname(__file__))
 MAJ = 0
 MIN = 0
-REV = 0
+REV = 1
 VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
 with open(os.path.join(curdir, 'sbwa/version.py'), 'w') as fout:
     fout.write(
@@ -21,7 +21,6 @@ setup(
     author_email='sag086@ucsd.edu',
     url='https://github.com/Samgarci17/CSE-185',
     packages=find_packages(),
-    python_requires=">=3.6",
     entry_points={
         "console_scripts": [
             'sbwa=sbwa.sbwa:main'
