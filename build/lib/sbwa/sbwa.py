6d53f69c4097cfcc5f3050c8eef84e9470fbece7
Samuel Garcia <sag086@ucsd.edu>
2023-06-10 04:52:49 -0700

    Final version

--- build/lib/sbwa/sbwa.py
+++ build/lib/sbwa/sbwa.py
@@ -4,9 +4,8 @@
 Command line script to create an index file from a dna dequence as well as auxillary data structues that assist with local alignment.
 
 '''
-from pathlib import Path
 import os
-from . import util as util
+from .util import * 
 import argparse
 import fastq as fq
 from dnazip.encoder import FullEncoder
@@ -27,25 +26,27 @@ def main():
 
     fasta_strings = mf.read(args.fa, seq=True)
     fasta_strings = list(fasta_strings)
-    f = open(outdir + 'sequence.txt', 'w')
+    f = open(outdir + '/sequence.txt', 'w')
     genome = fasta_strings[0]
     for x in fasta_strings:
         f.write(x)
     f.close()
-    encode = FullEncoder(outdir + r'sequence.txt')
+    encode = FullEncoder(outdir + r'/sequence.txt')
     encode.full_zip()
-    bwtmf = util.buildBWTMF(list(encode.bw_encoder.bwt), genome, 100)
+    bwtmf = buildBWTMF(list(encode.bw_encoder.bwt), genome, 100)
 
     
-    f = open(outdir + 'partialSuffixArray.txt', 'w')
+    f = open(outdir + '/partialSuffixArray.txt', 'w')
+    print(f)
     f.write(''.join(str(bwtmf[0])))
     f.close
-    f = open(outdir + 'occurenceArray.txt', 'w')
+    f = open(outdir + '/occurenceArray.txt', 'w')
     f.write(''.join(str(bwtmf[1])))
     f.close
-    f = open(outdir + 'countArray.txt', 'w')
+    f = open(outdir + '/countArray.txt', 'w')
     f.write(''.join(str(bwtmf[2])))
     f.close
+
 
 if __name__ == "__main__":
     main()
