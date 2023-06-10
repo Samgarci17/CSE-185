6fdac146a95a94b2a6075dc917f2ae043e476014
Samuel Garcia <sag086@ucsd.edu>
2023-06-10 01:31:02 -0700

    added setup for command line use

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sbwa',
+    version='0.0.2',
+    description='Index database sequences in the FASTA format.',
+    author='Samuel Garcia',
+    author_email='sag086@ucsd.edu',
+    url='https://github.com/Samgarci17/CSE-185',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            'sbwa=src.sbwa.sbwa:main'
+        ],
+    },
+)
